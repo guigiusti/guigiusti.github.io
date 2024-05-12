4d8edb9e62e5f1f275d8bad3e87d90b395c6574d
Guilherme <guigiusti@proton.me>
2024-05-12 11:42:03 -0300

    updates db

--- navbar.js
+++ navbar.js
@@ -5,19 +5,3 @@ $.get(httpsHost+"/nav.html", function(data){
 $.get(httpsHost+"/footer.html", function(data){
     $("#footerDiv").replaceWith(data);
 });
-function replaceimage (){
-    if (window.location.pathname != '/dev/' && window.location.pathname != '/dev') {
-        const img = document.createElement('img');
-        img.id = 'menu_img_file';
-        img.className = 'img_show'
-        img.src = httpsHost+'/media/background.JPG';
-        img.style.display = 'block';
-        document.querySelector('#blockline_menu').replaceWith(img);
-        const imgclasses = document.querySelectorAll('.img_show');
-        imgclasses.forEach(imgEl => {
-        imgEl.style.display = "block";
-        });
-    }
-
-}
-setTimeout(replaceimage, 500)
