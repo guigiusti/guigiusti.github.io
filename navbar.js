954769d4182d7c06a2bb838cf484e508b9361153
Guilherme <guigiusti@proton.me>
2024-05-31 16:28:33 -0300

    updates js

--- navbar.js
+++
@@ -1,7 +0,0 @@
-const httpsHost = "https://" + window.location.hostname;
-$.get(httpsHost+"/nav.html", function(data){
-    $("#nav-placeholder").replaceWith(data);
-});
-$.get(httpsHost+"/footer.html", function(data){
-    $("#footerDiv").replaceWith(data);
-});
